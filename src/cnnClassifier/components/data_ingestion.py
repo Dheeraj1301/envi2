74a18d5e4d020f3dfa856cfa1a6f983498d1be3e
dheeraj1301 <dheeraj1301@gmail.com>
2025-03-01 23:48:17 +0530

    Update2

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -1,4 +1,7 @@
+
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 import gdown
 import zipfile
 from src.config.configuration import ConfigurationManager
